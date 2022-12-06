6eea732eace852737d70640691ef2471dee95e26
sooriya <sooriya.n@iorta.in>
2022-12-06 13:17:23 +0530

    try catch added

--- controller.js
+++ controller.js
@@ -4,36 +4,65 @@ class TaskController {
     constructor() { }
 
     async create(req, res) {
-        const task = await new Task(req.body).save();
-        return res.status(200).json({ success: true, data: task, message: "New Task Created" });
+        try {
+            const task = await new Task(req.body).save();
+            return res.status(200).json({ success: true, data: task, message: "New Task Created" });
+        } catch (e) {
+            return res.status(400).json({ success: false, message: "Something Went Wrong !" });
+        }
     }
 
     async list(req, res) {
-        let list = await Task.find()
-        let count = await Task.find().countDocuments()
-
-        let output = {
-            list,
-            count,
+        try {
+            let list = await Task.find({ id: req.body.id })
+            let count = await Task.find({ id: req.body.id }).countDocuments()
+            let output = {
+                list,
+                count,
+            }
+            return res.status(200).json({ success: true, data: output, message: "Task Listed !" });
         }
-        return res.status(200).json({ success: true, data: output, message: "Task Listed !" });
+        catch (e) {
+            return res.status(400).json({ success: false, message: "Something Went Wrong !" });
+        }
     }
 
     async update(req, res, next) {
-        let task = await Task.findOne({ _id: req.body.id });
-        let updates = Object.keys(req.body)
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
-        res.status(200).send({ success: true, data: task, message: "Successfully Updated task Details " })
+        try {
+            let task = await Task.findOne({ _id: req.body.id });
+            let updates = Object.keys(req.body)
+            updates.forEach((update) => task[update] = req.body[update])
+            await task.save()
+            res.status(200).send({ success: true, data: task, message: "Successfully Updated task Details " })
+        }
+        catch (e) {
+            return res.status(400).json({ success: false, message: "Something Went Wrong !" });
+        }
     }
 
+    //soft delete
     async delete(req, res) {
-        let remove = await Task.findOne(req.body.id)
-        remove.deleted = true;
-        await remove.save();
-        return res.status(200).json({ success: true, data: remove, message: "task delete" })
+        try {
+            let remove = await Task.findOne(req.body.id)
+            remove.deleted = true;
+            await remove.save();
+            return res.status(200).json({ success: true, data: remove, message: "task delete" })
+        }
+        catch (e) {
+            return res.status(400).json({ success: false, message: "Something Went Wrong !" });
+        }
     }
 
+    //hard delete
+    async delete(req, res) {
+        try {
+            await Task.deleteOne({ _id: req.body.id })
+            return res.status(200).json({ success: true, message: "Task deleted permanently" });
+        }
+        catch (e) {
+            return res.status(400).json({ success: false, message: "Something Went Wrong !" });
+        }
+    }
 }
 
 module.exports = TaskController
